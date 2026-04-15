5cbf8bf08e9818a4c6b15a77ab2cb73d46b3ec6d
RonDevHub <no_reply@rondev.de>
2026-04-15 16:46:05 +0000

    Add includes/header.php

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title><?php echo $pageTitle; ?></title>
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/themes.css">
+    <link rel="manifest" href="manifest.json">
+    <meta name="theme-color" content="#0f380f">
+</head>
+<body class="theme-gameboy">
