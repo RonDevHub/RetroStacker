99bed62bc8f3ea91d35cd5eebdeea017f7e6a14f
RonDevHub <no_reply@rondev.de>
2026-04-16 06:23:03 +0000

    includes/header.php aktualisiert

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title><?php echo $pageTitle; ?></title>
+    <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="assets/css/main.css">
     <link rel="stylesheet" href="assets/css/themes.css">
     <link rel="manifest" href="manifest.json">
